bce80937f5831aed04b05456a41a5ff3f50aba7d
Renee Pinna <renee.pinna@gmail.com>
2023-10-31 10:13:44 -0600

    Add homepage element

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,15 @@
 
 //b8fc9fa1e6694440bd9372d8f6ada677
 
+import { Route, Routes } from "react-router-dom";
+import Homepage from "../Homepage/Homepage";
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Routes>
+      <Route path="/" element={<Homepage/>}/>
+    </Routes>
+    
   );
 }
 
---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,14 @@
+export default function Homepage() {
+  return (
+    <div>
+      <header>
+        <p>News Reader</p>
+        <p>Search Comp</p>
+      </header>
+      <main>
+      
+      Homepage
+      </main>
+    </div>
+  )
+}
