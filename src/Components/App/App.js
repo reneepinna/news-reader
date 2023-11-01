db38c52a6200e59b6d505b507f666b320fe2d9b3
Renee Pinna <renee.pinna@gmail.com>
2023-11-01 13:36:25 -0600

    Add Article component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,3 @@
-//b8fc9fa1e6694440bd9372d8f6ada677
-
 import { Route, Routes } from 'react-router-dom';
 import Homepage from '../Homepage/Homepage';
 import Header from '../Header/Header';
--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -1,16 +1,41 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import './Article.scss';
+import { useLocation } from 'react-router-dom';
+import dayjs from 'dayjs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 export default function Article() {
-  const {state} = useLocation()
-  const {article} = state;
-
-
+  const { state } = useLocation();
+  const { article } = state;
+  const {
+    source,
+    author,
+    url,
+    title,
+    description,
+    urlToImage,
+    publishedAt,
+    content,
+  } = article;
+  const navigate = useNavigate()
+  const date = dayjs(publishedAt).format('dddd, MMMM D, YYYY h:mm A	');
 
   return (
-    <div>
-      <p>{article.title}</p>
-      
-    </div>
-  )
+    <main className='article-page'>
+      <article className='article-body'>
+      <div className='icon-left' onClick={() => navigate('/')}>
+        <FontAwesomeIcon icon={faArrowLeft} size='xl' back/>
+      </div>
+        <h2 className='title'>{title.split(' -')[0]}</h2>
+        <p>{description}</p>
+        <p className='author'>By {author.toLowerCase()}</p>
+        <p className='date'>{date}</p>
+        <hr class='separator' />
+        <img className='article_img' src={urlToImage} />
+        <p className='source'>{source.name}</p>
+        <p>{content}</p>
+      </article>
+    </main>
+  );
 }
