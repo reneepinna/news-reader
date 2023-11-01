20f9d0055af1a0fcc435377b487fee061c579f86
Renee Pinna <renee.pinna@gmail.com>
2023-10-31 22:19:10 -0600

    Create article card

--- src/Components/ListArticle/ListArticle.js
+++ src/Components/ListArticle/ListArticle.js
@@ -1,21 +1,22 @@
-import './ListArticle.scss'
+import './ListArticle.scss';
 import dayjs from 'dayjs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function ListArticle({ article }) {
-  const {
-    title,
-    description,
-    urlToImage,
-    publishedAt,
-  } = article;
+  const { title, description, urlToImage, publishedAt } = article;
 
-  const date = dayjs(publishedAt).format('DD/MM/YYYY')
+  const date = dayjs(publishedAt).format('DD/MM/YYYY');
 
-  return <article className="listArticle">
-    <img className="listArticle_img" src={urlToImage}/>
-    <h3 className="">{title}</h3>
-    <p className="listArticle_text">{description}</p>
-    <p className="listArticle_text">{date}</p>
-    <a></a>
-  </article>;
+  return (
+    <article className='listArticle'>
+      <img className='listArticle_img' src={urlToImage} />
+      <h3 className=''>{title}</h3>
+      <p className='listArticle_text'>{description}</p>
+      <p className='listArticle_date'>{date}</p>
+      <div className='icon'>
+        <FontAwesomeIcon icon={faArrowRight} color='#B20710' size='xl' back/>
+      </div>
+    </article>
+  );
 }
