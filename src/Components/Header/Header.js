35505a529e48f727ece592333ec151e89a2f005d
Renee Pinna <renee.pinna@gmail.com>
2023-11-02 18:39:59 -0600

    Restyle header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,12 @@
-import { NavLink } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 import './Header.scss';
 import SearchBar from '../SeachBar/SearchBar';
 
 export default function Header() {
+  const navigate = useNavigate()
   return (
     <nav className='header'>
-      <h1>News Reader</h1>
+      <h1 className='logo' onClick={() => navigate('')}>News Reader</h1>
       <SearchBar />
     </nav>
   );
